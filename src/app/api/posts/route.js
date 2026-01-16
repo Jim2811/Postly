ea34b42b747a9b97fd8189f98f0a0e4707429cc4
Rukhsat <rhjim2005@gmail.com>
2026-01-16 23:12:10 +0600

    Created Post api for adding posts

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -6,3 +6,9 @@ export async function GET() {
     const posts = await db.collection("posts").find({}).toArray()
     return NextResponse.json(posts)
 }
+export async function POST(request) {
+    const db = await connectDB();
+    const body = await request.json()
+    const result = await db.collection("posts").insertOne(body)
+    return NextResponse.json({insertedId : result.insertedId})
+}
